c7c30ba905af5c4d3db9bdb2a74c227038b28e77
richard-home <402822848@qq.com>
2023-10-08 11:12:47 +0800

    host:localhost

--- back-end/env.js
+++ back-end/env.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const APP_HOST = '8.134.152.216';
+const APP_HOST = 'localhost';
 const APP_PORT = 8080;
 const DATABASE_SETTING = {
-	HOST: '8.134.152.216',
+	HOST: 'localhost',
 	PORT: 3306,
 	USER: 'root',
 	PASSWORD: 'Cw94618!',
